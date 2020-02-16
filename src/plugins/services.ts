5c2ecd307b3fa9db59fb2503ed6df7e3d5cc1a95
Paul Gadi <paulgadi@gmail.com>
2020-02-16 09:55:37 -0700

    Update services.ts

--- src/plugins/services.ts
+++ src/plugins/services.ts
@@ -38,14 +38,14 @@ const servicesPlugin: Plugin = ({ app, isDev }, inject) => {
     app.$axios,
     isDev
   )
-  const ethersService = new EthersService(
-    app.$axios,
-    isDev
-  )
+  // const ethersService = new EthersService(
+  //   app.$axios,
+  //   isDev
+  // )
 
   inject('openSeaService', openSeaService)
   inject('threeBoxService', threeBoxService)
-  inject('ethersService', ethersService)
+  // inject('ethersService', ethersService)
 }
 
 export default servicesPlugin
